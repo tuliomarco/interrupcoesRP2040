d3cf1b9129172ce794b8d0d9ad66621f59eebbef
Marco Túlio Macêdo <marcomacedo018@gmail.com>
2025-02-01 17:38:59 -0300

    Inclusão das rotinas para controle dos botões

--- interrupcoesRP2040.c
+++ interrupcoesRP2040.c
@@ -19,6 +19,10 @@
 #define LED_MTX_LEVEL 10
 #define LED_MTX_PIN 7
 
+// Definições relacionadas ao controle de tempo e debounce
+#define DEBOUNCE_TIME 100
+#define TIME to_ms_since_boot(get_absolute_time())
+
 // Definição do pixel GRB
 typedef struct pixel_t {
     uint8_t G, R, B;
@@ -60,18 +64,26 @@ void init_pio(uint pin) {
  * Função de inicialização das GPIOs
  */
 void init_gpio() {
+    // Inicializa os pinos GPIO dos LEDs e botões
     gpio_init(LED_R_PIN);
     gpio_init(LED_G_PIN);
     gpio_init(LED_B_PIN);
     gpio_init(BTN_A_PIN);
     gpio_init(BTN_B_PIN);
 
+    // Define LEDs como saída e botões como entrada
     gpio_set_dir(LED_R_PIN, GPIO_OUT);
     gpio_set_dir(LED_G_PIN, GPIO_OUT);
     gpio_set_dir(LED_B_PIN, GPIO_OUT);
+
     gpio_set_dir(BTN_A_PIN, GPIO_IN);
     gpio_set_dir(BTN_B_PIN, GPIO_IN);
 
+    // Habilita resistores de pull-up nos botões
+    gpio_pull_up(BTN_A_PIN);
+    gpio_pull_up(BTN_B_PIN);
+
+    // Inicializa LEDs apagados
     gpio_put(LED_R_PIN, 0);
     gpio_put(LED_G_PIN, 0);
     gpio_put(LED_B_PIN, 0);
@@ -124,13 +136,39 @@ void write_leds() {
     }
 }
 
+volatile int number = 0; // Variável global para armazenar o número atual
+volatile unsigned long debounce_time; // Variável para controle de debounce
+
+void button_callback(uint gpio, uint32_t events) {
+    // Verifica se o tempo desde o último acionamento é maior que o tempo de debounce
+    if(TIME - debounce_time > DEBOUNCE_TIME) {   
+        if (events & GPIO_IRQ_EDGE_FALL) { // Verifica se o evento foi uma borda de descida (pressionamento do botão)
+            if (gpio == BTN_A_PIN) {
+                number = (number + 1) % 10; // Incrementa o número, retornando a 0 se ultrapassar 9
+            } else if (gpio == BTN_B_PIN) {
+                number = (number - 1 + 10) % 10; // Decrementa o número, voltando para 9 se for menor que 0
+            }
+            set_led_by_pattern(led_number_pattern[number]);
+            write_leds(); 
+        }
+    }
+}
+
 int main() {
     stdio_init_all();
     init_pio(LED_MTX_PIN);
     clear_leds();
     init_gpio();
-    
+
+    set_led_by_pattern(led_number_pattern[number]); // Atualiza o padrão de LEDs (Inicia como 0)
+    write_leds(); // Envia os dados para os LEDs
+
+    // Configura interrupções para os botões com um callback único
+    gpio_set_irq_enabled_with_callback(BTN_A_PIN, GPIO_IRQ_EDGE_FALL, true, &button_callback);
+    gpio_set_irq_enabled_with_callback(BTN_B_PIN, GPIO_IRQ_EDGE_FALL, true, &button_callback);
+
     while (true) {
+        // Pisca o LED vermelho 5 vezes por segundo
         sleep_ms(100);
         gpio_put(LED_R_PIN, 0);
         sleep_ms(100);
